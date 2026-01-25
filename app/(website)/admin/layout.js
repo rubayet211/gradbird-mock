e825ee23d12321d04bcc1d0d97c051ccc3fe159a
Rhyme Rubayet <rubayet211@gmail.com>
2026-01-25 13:17:46 +0600

    feat: Enable dynamic rendering in AdminLayout component

--- app/(website)/admin/layout.js
+++ app/(website)/admin/layout.js
@@ -1,5 +1,7 @@
 import Link from "next/link";
 
+export const dynamic = 'force-dynamic';
+
 export default function AdminLayout({ children }) {
     return (
         <div className="min-h-screen bg-gray-50 flex flex-col md:flex-row">
